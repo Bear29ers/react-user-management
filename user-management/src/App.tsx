7837796e08913c16c0d650b93b2f6baa40b43e26
Yuta Okuma <y.dream.nest@icloud.com>
2022-08-31 09:25:14 +0900

    ルーティングの設定

--- user-management/src/App.tsx
+++ user-management/src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { ChakraProvider, Button } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './router/Router';
+
 import theme from './theme/theme';
 import './App.css';
 
 export default function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Button colorScheme="teal">ボタン</Button>
-      <p>ああああああ</p>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
