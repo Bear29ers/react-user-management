94e6451d50acffeac7f14620f7feafcd064a8fae
Yuta Okuma <y.dream.nest@icloud.com>
2022-09-01 09:23:38 +0900

    ヘッダーの作成

--- user-management/src/router/Router.tsx
+++ user-management/src/router/Router.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Login } from '../components/pages/Login';
 import { homeRoutes } from './HomeRoutes';
 import { Page404 } from '../components/pages/Page404';
+import { HeaderLayout } from '../components/template/HeaderLayout';
 
 export const Router: FC = memo(() => {
   return (
@@ -11,7 +12,7 @@ export const Router: FC = memo(() => {
       <Route path='/' element={<Login />} />
       <Route path='/home'>
         {homeRoutes.map((route) => (
-          <Route key={route.path} path={`/home${route.path}`} element={route.element} />
+          <Route key={route.path} path={`/home${route.path}`} element={<HeaderLayout>{route.element}</HeaderLayout>} />
         ))}
       </Route>
       <Route path='*' element={<Page404 />} />
