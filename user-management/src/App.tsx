d35fd8909ed9d0411d5d0fadd4e8828e1e2a80e6
Yuta Okuma <y.dream.nest@icloud.com>
2022-09-19 10:40:19 +0900

    グローバルなスタイルを設定する

--- user-management/src/App.tsx
+++ user-management/src/App.tsx
@@ -1,8 +1,11 @@
 import { Button, ChakraProvider } from '@chakra-ui/react';
 import React, { FC, memo } from 'react';
 
+import { theme } from './theme/theme';
+
 export const App: FC = memo(() => (
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <Button colorScheme="teal">ボタン</Button>
+    <p>あああああああ</p>
   </ChakraProvider>
 ));
