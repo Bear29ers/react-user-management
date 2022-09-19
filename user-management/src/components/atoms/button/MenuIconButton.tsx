307d1f3c9029a7e0946c74942f23a74092d709ed
Yuta Okuma <y.dream.nest@icloud.com>
2022-09-19 19:17:31 +0900

    ヘッダーの作成（コンポーネント分割）

---
+++ user-management/src/components/atoms/button/MenuIconButton.tsx
@@ -0,0 +1,21 @@
+import React, { FC, memo } from "react";
+import { IconButton } from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+
+type Props = {
+  onOpen: () => void;
+};
+
+export const MenuIconButton: FC<Props> = memo((props) => {
+  const { onOpen } = props;
+  return (
+    <IconButton
+      aria-label="メニューボタン"
+      icon={<HamburgerIcon />}
+      size="md"
+      variant="unstyled"
+      display={{ base: "block", md: "none" }}
+      onClick={onOpen}
+    />
+  );
+})
